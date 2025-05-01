9a2d04e2ac9cc21da2a2abe0422fa04fe5483787
Ricard A <rricarrd@gmail.com>
2025-05-01 20:13:31 +0200

    RunAllTmux

--- runAllCases.sh
+++ runAllCases.sh
@@ -1,13 +1,10 @@
-# Attach new tmux session
-tmux new -s myfoamrun
-
 # Executing all the runCase.sh options
 for dir in *; do
     if [ -f "$dir/runCase.sh" ]; then
     	
     	cd $dir
         echo "Running $dir/runCase.sh using foamJob"
-        bash runCase.sh
+        bash runCaseTmux.sh
         cd ..
     fi
 done
